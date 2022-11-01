f5a8aa17c72edaba454011654997120816d93af2
Tom Booth <tombeezle@gmail.com>
2022-11-01 15:11:22 -0700

    first test passing for displaying all items

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,14 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { zodiacs } = require('../lib/zodiacs-data');
+
 describe('zodiacs routes', () => {
   it('/zodiacs should return a list of zodiacs', async () => {
     const res = await request(app).get('/zodiacs');
-    const expected = zodiac.map((zodiac) => {
+    const expected = zodiacs.map((zodiac) => {
       return { id: zodiac.id, name: zodiac.name };
     });
     expect(res.body).toEqual(expected);
   });
+});
